3466407903d075c96369e64f557a3ee9253d1875
HanSeongLee <jkhanseong@gmail.com>
2023-12-13 18:33:26 +0900

    feat: add font

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,8 +1,24 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { Plus_Jakarta_Sans } from 'next/font/google';
+
+const PlusJakartaSansFont = Plus_Jakarta_Sans({
+  weight: ['500', '700'],
+  subsets: ['latin-ext'],
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+      <>
+        <style jsx global>{`
+            :root {
+              --plus-jakarta-sans-font: ${PlusJakartaSansFont.style.fontFamily};
+            }
+        `}
+        </style>
+        <Component {...pageProps} />
+      </>
+  );
 }
 
 export default MyApp
