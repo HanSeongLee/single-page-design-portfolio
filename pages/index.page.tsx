642deb1518dcb2d2102330fcb9c99977897f4f29
HanSeongLee <jkhanseong@gmail.com>
2023-12-22 18:00:59 +0900

    feat: add main page

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -1,12 +1,24 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
+import styles from './style.module.scss';
+import Header from 'components/commons/Header';
+import HeroSection from 'components/sections/HeroSection';
+import IntroSection from 'components/sections/IntroSection';
+import MyWorkSection from 'components/sections/MyWorkSection';
+import CTASection from 'components/sections/CTASection';
 
 const Home: NextPage = () => {
-  return (
-      <>
-        <main>
-        </main>
-      </>
-  );
-}
+    return (
+        <>
+            <Header />
+            <main className={styles.main}>
+                <HeroSection />
+                <IntroSection />
+                <MyWorkSection />
+                <CTASection />
+            </main>
+            <Header type={'footer'} />
+        </>
+    );
+};
 
 export default Home
